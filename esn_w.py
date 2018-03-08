a2c991825f5f137cd0a378f48af0fc20e18e4fa9
nschaetti <n.schaetti@gmail.com>
2018-03-08 17:38:30 +0100

    ESN-W

--- esn_w.py
+++ esn_w.py
@@ -5,24 +5,52 @@
 import torch.utils.data
 import dataset
 from echotorch.transforms import text
-import matplotlib.pyplot as plt
 
+# Experience parameter
+batch_size = 64
+n_epoch = 1
+window_size = 700
+training_set_size = 10
+test_set_size = 2
+training_samples = training_set_size + test_set_size
+stride = 100
 
-# Style change detection dataset
-pan18loader = torch.utils.data.DataLoader(
-    dataset.StyleChangeDetectionDataset(root='./data/', download=True, transform=text.GloveVector(), train=False,
-                                        point_form='gauss', sigma=10)
+# Style change detection dataset, training set
+pan18loader_train = torch.utils.data.DataLoader(
+    dataset.SCDSimpleDataset(root='./data/', download=True, transform=text.GloveVector(), train=True),
+    batch_size=1
+)
+
+# Style change detection dataset, validation set
+pan18loader_valid = torch.utils.data.DataLoader(
+    dataset.SCDSimpleDataset(root='./data/', download=True, transform=text.GloveVector(), train=False),
+    batch_size=1
 )
 
 # Get training data
-for i, data in enumerate(pan18loader):
-    # Inputs and labels
-    inputs, labels, c = data
+for i, data in enumerate(pan18loader_train):
+    # Inputs and c
+    inputs, label = data
 
-    # Print
-    print(u"Inputs : {}".format(inputs.size()))
-    print(u"Labels : {}".format(labels.size()))
-    print(u"Class : {}".format(c))
+    # TRAINING
 # end for
 
-print(pan18loader.dataset.changes_char)
+# Test loss
+successes = 0.0
+total = 0.0
+
+# Validation
+for i, data in enumerate(pan18loader_valid):
+    # Inputs and c
+    inputs, label = data
+
+    # EXEC
+
+    # Counter
+    total += 1.0
+# end for
+
+# Show
+print(u"Validation {}".format(
+    100.0 * successes / total
+))
