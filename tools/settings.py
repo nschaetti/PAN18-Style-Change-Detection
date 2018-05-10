f469247fb2d7eff25028a1207f2827a8ac534ab0
nschaetti <n.schaetti@gmail.com>
2018-05-10 15:30:17 +0200

    Bi

--- tools/settings.py
+++ tools/settings.py
@@ -11,9 +11,9 @@ from torchvision import transforms
 ################
 
 # Settings
-cnn_window_size = 11400
+cnn_window_size = 12000
 gru_window_size = 20
-hidden_dim = 500
+hidden_dim = 250
 voc_sizes = {'c1': 1628, 'c2': 21510}
 class_to_idx = {False: 0, True: 1}
 idx_to_class = {0: False, 1: True}
